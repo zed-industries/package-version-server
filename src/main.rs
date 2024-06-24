4dc51e4ddb6f2bd9845f92bd0bf46414d55e471c
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-06-24 21:17:23 +0200

    Use tree-sitter queries instead of regex + track unsaved file changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+
 use chrono::{DateTime, FixedOffset};
 use chrono_humanize::{Accuracy, HumanTime, Tense};
 use regex::Regex;
@@ -5,39 +8,74 @@ use serde_json::{self, Value};
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
+use tree_sitter::{Parser, Point, Query, QueryCursor};
+use tree_sitter_json::language;
 
 struct Backend {
     client: Client,
-    package_lock_version_regex: Regex,
+    file_contents: Arc<Mutex<HashMap<Url, Arc<str>>>>,
 }
 
 impl Backend {
     fn new(client: Client) -> Result<Self> {
         Ok(Self {
             client,
-            package_lock_version_regex: Regex::new(r#""([\w-]+)":\s*"([\w.-]+)""#)
-                .map_err(|_| tower_lsp::jsonrpc::Error::internal_error())?,
+            file_contents: Default::default(),
         })
     }
+    fn extract_package_name(text: Arc<str>, position: Position) -> Option<String> {
+        let mut parser = Parser::new();
+        parser.set_language(&language()).ok()?;
 
-    fn extract_package_name_and_version(
-        &self,
-        document: &str,
-        position: Position,
-    ) -> Option<(String, String)> {
-        let lines: Vec<&str> = document.lines().collect();
-        let line = lines.get(position.line as usize)?;
+        let tree = parser.parse(text.as_bytes(), None)?;
+        let point = Point {
+            row: position.line as usize,
+            column: position.character as usize,
+        };
 
-        if let Some(matches) = self.package_lock_version_regex.captures(line) {
-            if matches.len() > 2 {
-                let name = matches.get(1)?.as_str().to_string();
-                let version = matches.get(2)?.as_str().to_string();
-                return Some((name, version));
+        let query_str = r#"
+            (pair
+                key: (string (string_content) @root_name)
+                value:
+                    (object
+                        (pair
+                            key: (string (string_content) @name)
+                            value: (string)
+                        )
+                    )
+                (#any-of? @root_name "dependencies" "devDependencies" "peerDependencies" "optionalDependencies" "bundledDependencies" "bundleDependencies")
+            )+
+        "#;
+
+        let query = Query::new(&language(), query_str).ok()?;
+        let mut cursor = QueryCursor::new();
+
+        let root_node = tree.root_node();
+        let matches = cursor.matches(&query, root_node, text.as_bytes());
+        let mut package_name = None;
+        let capture_names = query.capture_names();
+        for m in matches {
+            package_name.take();
+            let mut matched = false;
+            for capture in m.captures {
+                let capture_name = capture_names[capture.index as usize];
+                if capture_name == "name" {
+                    package_name = Some(capture.node.utf8_text(text.as_bytes()).ok()?.to_string());
+                }
+                let node_range = capture.node.range();
+                if node_range.start_point <= point && node_range.end_point >= point {
+                    matched = true;
+                }
+            }
+            if matched {
+                return package_name;
             }
         }
-        None
+
+        package_name
     }
 }
+
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
@@ -63,19 +101,36 @@ impl LanguageServer for Backend {
             .await;
     }
 
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        if let Some(change) = params.content_changes.into_iter().next() {
+            self.file_contents
+                .lock()
+                .unwrap()
+                .insert(params.text_document.uri, change.text.into());
+        }
+    }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        self.file_contents
+            .lock()
+            .unwrap()
+            .insert(params.text_document.uri, params.text_document.text.into());
+    }
+
     async fn hover(&self, params: HoverParams) -> Result<Option<Hover>> {
         let uri = params.text_document_position_params.text_document.uri;
 
         if !uri.path().ends_with("package.json") {
             return Ok(None);
         }
-        let document = std::fs::read_to_string(uri.to_file_path().unwrap()).unwrap();
-        let package_name_pair = self.extract_package_name_and_version(
-            &document,
-            params.text_document_position_params.position,
-        );
+        let Some(document) = self.file_contents.lock().unwrap().get(&uri).cloned() else {
+            return Ok(None);
+        };
 
-        let Some((package_name, _)) = package_name_pair else {
+        let package_name_pair =
+            Self::extract_package_name(document, params.text_document_position_params.position);
+
+        let Some(package_name) = package_name_pair else {
             return Ok(None);
         };
         let meta = fetch_latest_version(&package_name)
@@ -132,4 +187,31 @@ async fn main() {
     let (service, socket) =
         LspService::new(|client| Backend::new(client).expect("Failed to initialize backend"));
     Server::new(stdin, stdout, socket).serve(service).await;
+}
+
+#[cfg(test)]
+mod tests {
+    use tower_lsp::lsp_types::Position;
+
+    use crate::Backend;
+
+    #[test]
+    fn test_parse_package_json() {
+        let package = r#"{
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+}
+"#;
+        assert_eq!(
+            Backend::extract_package_name(
+                package.into(),
+                Position {
+                    line: 2,
+                    character: 11,
+                },
+            ),
+            Some("express".into())
+        );
+    }
 }
